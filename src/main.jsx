5c6c5b4b1e37471e60dc40dc05a310b2d6acb87b
HeyBoY-ops <abhi9835003292@gmail.com>
2025-05-30 14:43:53 +0530

    ..

--- src/main.jsx
+++ src/main.jsx
@@ -1,44 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
-import { UserProvider } from "./context/UserContext";  // <-- import your UserProvider
+import { HashRouter } from "react-router-dom";
+import { UserProvider } from "./context/UserContext";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <UserProvider>
         <App />
       </UserProvider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
-
-
-
-{/*ReactDOM.createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <UserProvider>
-      <App />
-    </UserProvider>
-  </BrowserRouter>
-);*/}
-
-
-
-{/*import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
-import { UserProvider } from "./context/UserContext";
-
-
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-  <UserProvider>
-      <App />
-  </UserProvider>
-  </BrowserRouter>
-)*/}
 
