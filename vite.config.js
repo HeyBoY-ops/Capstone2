e919babd4af94ada8e1621c279a730b680c6329f
HeyBoY-ops <abhi9835003292@gmail.com>
2026-01-12 20:08:59 +0530

    Fix Deployment Paths.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,6 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/Capstone2/',
-  plugins: [react()],
-});
+  base: process.env.VERCEL ? '/' : '/Budget-Planner/'
+})
